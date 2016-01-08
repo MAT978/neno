a7fa17b9d0214ea74068a0d6d82d997ba47874f4
vistiyos <victor@notwebdesign.com>
2016-01-08 15:53:28 +0100

    Preparing data structure for ACL

--- administrator/components/com_neno/sql/updates/1.2.3.sql
+++ administrator/components/com_neno/sql/updates/1.2.3.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `#__neno_content_element_table_filters` DROP INDEX table_id;
 ALTER TABLE `#__neno_content_element_language_files` ADD `translate` TINYINT(1) NOT NULL DEFAULT '1';
 ALTER TABLE `#__neno_content_element_language_strings` ADD `translate` TINYINT(1) NOT NULL DEFAULT '1';
+ALTER TABLE `#__neno_content_element_translations` ADD `checked_out` INT NOT NULL;
+ALTER TABLE `#__neno_content_element_translations` ADD `checked_out_time` DATETIME NOT NULL;
