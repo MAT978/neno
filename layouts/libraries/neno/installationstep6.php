4fa267aba43ad4db8943c611971a35b55dc7abdd
Victor Escobar <victor@notwebdesign.com>
2016-04-18 15:15:47 +0200

    Fixing issue showing wrong step on the installation screen.

--- layouts/libraries/neno/installationstep6.php
+++ layouts/libraries/neno/installationstep6.php
@@ -37,5 +37,5 @@ JHtml::_('bootstrap.tooltip');
 		</div>
 	</div>
 
-	<?php echo JLayoutHelper::render('installationbottom', 5, JPATH_NENO_LAYOUTS); ?>
+	<?php echo JLayoutHelper::render('installationbottom', 6, JPATH_NENO_LAYOUTS); ?>
 </div>
