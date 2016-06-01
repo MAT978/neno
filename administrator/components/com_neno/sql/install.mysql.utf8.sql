b0612ad419ef69c8843c3e0f4f570ac9291ed876
Juan Sánchez <juan@notwebdesign.com>
2016-06-01 16:04:14 +0200

    Adding fields to db

--- administrator/components/com_neno/sql/install.mysql.utf8.sql
+++ administrator/components/com_neno/sql/install.mysql.utf8.sql
@@ -377,17 +377,19 @@ CREATE TABLE IF NOT EXISTS `#__neno_translation_states` (
   `description` VARCHAR(255) NULL,
   PRIMARY KEY (`id`));
 
-
 --
 -- Table structure for table `#__neno_content_issues`
 --
 CREATE TABLE `#__neno_content_issues` (
-  `id`          INT(11)     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `discovered`  DATETIME    NOT NULL,
-  `error_code`  VARCHAR(45) NOT NULL,
-  `info`        TEXT        NOT NULL,
-  `fixed`       DATETIME    NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `fixed_by`    INT         NOT NULL
+  `id`          INT(11)       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `discovered`  DATETIME      NOT NULL,
+  `error_code`  VARCHAR(45)   NOT NULL,
+  `item_id`     VARCHAR(11)   DEFAULT NULL,
+  `extension`   VARCHAR(255)  DEFAULT NULL,
+  `lang`        VARCHAR(8)    DEFAULT NULL,
+  `info`        TEXT          NOT NULL,
+  `fixed`       DATETIME      NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `fixed_by`    INT(11)       NOT NULL DEFAULT '0'
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
