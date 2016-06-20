e96df2776a9389117db78682381c7e055eb83a4c
Juan Sánchez <juan@notwebdesign.com>
2016-06-20 10:48:18 +0200

    SQL files to remove machine translation method

--- administrator/components/com_neno/sql/install.mysql.utf8.sql
+++ administrator/components/com_neno/sql/install.mysql.utf8.sql
@@ -361,7 +361,6 @@ CREATE TABLE IF NOT EXISTS `#__neno_language_pairs_pricing` (
   `id`               INT(11)        NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `target_language`  VARCHAR(6)     NOT NULL,
   `translation_type` VARCHAR(25)    NOT NULL,
-  `price_per_word`   DECIMAL(6, 4) NOT NULL,
   `time_updated`     DATETIME       NOT NULL,
   UNIQUE (`target_language`, `translation_type`)
 );
---
+++ administrator/components/com_neno/sql/updates/2.1.2.sql
@@ -0,0 +1,15 @@
+---
+--- Change any machine translation method to manual
+---
+UPDATE `#__neno_content_element_groups_x_translation_methods`
+SET `translation_method_id` = 1
+WHERE (`translation_method_id` = 2);
+
+UPDATE `#__neno_content_element_translation_x_translation_methods`
+SET `translation_method_id` = 1
+WHERE (`translation_method_id` = 2);
+
+---
+--- Remove machine translation method
+---
+DELETE FROM `#__neno_translation_methods` WHERE (`id` = 2);
