6af4eb6804a1a9ee33ba9df7de1b9effa51f5810
Victor Escobar <victor@notwebdesign.com>
2016-07-13 12:28:06 +0200

    Creating plugin base class.

---
+++ libraries/neno/plugin/plugin.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * @package    Neno
+ * @subpackage Plugin
+ *
+ * @copyright  Copyright (c) 2016 Jensen Technologies S.L. All rights reserved
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+jimport('joomla.filesystem.file');
+
+/**
+ * Class NenoPlugin
+ */
+abstract class NenoPlugin extends JPlugin
+{
+	/**
+	 * Translation method plugin
+	 */
+	const TRANSLATION_METHOD_PLUGIN = 1;
+	/************** ABSTRACT METHODS ********************/
+
+	/**
+	 * Get plugin type
+	 *
+	 * @return string
+	 *
+	 * @see Constants
+	 */
+	abstract public function getType();
+
+	/**
+	 * @return JDatabaseQuery
+	 */
+	abstract protected function getListQueryForInterface();
+
+	/**
+	 * @return string
+	 */
+	abstract protected function getLayoutForInterface();
+
+	/**
+	 * Get Neno plugins by type
+	 *
+	 * @param int $pluginType Plugin type
+	 *
+	 * @see Constants
+	 *
+	 * @return array
+	 */
+	public static function getPluginsByType($pluginType)
+	{
+		$nenoPlugins = JPluginHelper::getPlugin('neno');
+		$plugins     = array();
+
+		foreach ($nenoPlugins as $nenoPlugin)
+		{
+			$className = 'plgNeno' . ucfirst($nenoPlugin);
+
+			if (class_exists($className))
+			{
+				/* @var $plugin NenoPlugin */
+				$plugin = new $className;
+
+				if (static::isPluginType($plugin->getType(), $pluginType))
+				{
+					$plugins[] = $plugin;
+				}
+			}
+		}
+
+		return $plugins;
+	}
+
+	/**
+	 * Render interface for this plugin
+	 *
+	 * @param int $limit
+	 * @param int $offset
+	 *
+	 * @return string
+	 */
+	public function onInterfaceRender($limit, $offset)
+	{
+		$db = JFactory::getDbo();
+		$db->setQuery($this->getListQueryForInterface(), $offset, $limit);
+		$results = $db->loadObjectList();
+
+		$layoutFile     = $this->getLayoutForInterface();
+		$layoutFileName = basename($layoutFile);
+		$layoutPath     = str_replace(DIRECTORY_SEPARATOR . $layoutFileName, '', $layoutFile);
+
+		return JLayoutHelper::render(JFile::stripExt($layoutFileName), $results, $layoutPath);
+	}
+
+	/**
+	 * Check if the plugin is of a certain type
+	 *
+	 * @param int $pluginType    Plugin type
+	 * @param int $pluginToCheck Plugin to check
+	 *
+	 * @return bool
+	 */
+	protected static function isPluginType($pluginType, $pluginToCheck)
+	{
+		return (!$pluginType ^ $pluginToCheck) === $pluginType;
+	}
+}
