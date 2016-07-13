d44b7776fd100bfd3d251bc1bcb4d54037848af7
Victor Escobar <victor@notwebdesign.com>
2016-07-13 16:35:26 +0200

    Adding translation interface.

---
+++ libraries/neno/plugin/interface/translation/method.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @package    Neno
+ * @subpackage Plugin
+ *
+ * @copyright  Copyright (c) 2016 Jensen Technologies S.L. All rights reserved
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+/**
+ * Class NenoPluginInterfaceTranslationMethod
+ */
+class NenoPluginInterfaceTranslationMethod
+{
+}
