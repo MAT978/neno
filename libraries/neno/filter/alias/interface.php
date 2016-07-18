d9a870c4e51f6aba9f30a1637a748ddb51e84c24
vistiyos <victor@notwebdesign.com>
2016-07-18 17:28:36 +0200

    Add transliterate interface.

---
+++ libraries/neno/filter/alias/interface.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package     Neno
+ * @subpackage  Filter
+ *
+ * @copyright   Copyright (c) 2014 Jensen Technologies S.L. All rights reserved
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+
+interface NenoFilterAliasInterface
+{
+
+}
