e9ccf260e84b5be6d9115879e812a42b81f1f338
vistiyos <victor@notwebdesign.com>
2016-07-29 12:30:29 +0200

    Render plugin buttons and handle correctly their actions.

--- administrator/components/com_neno/views/plgrender/tmpl/default.php
+++ administrator/components/com_neno/views/plgrender/tmpl/default.php
@@ -9,12 +9,34 @@
 
 defined('_JEXEC') or die;
 
+$input     = JFactory::getApplication()->input;
+$plugin    = $input->getCmd('plugin');
+$plgrender = $input->getCmd('plgrender');
+
+$formAction = JRoute::_("index.php?option=com_neno&view=plgrender&plugin=$plugin&plgrender=$plgrender");
 
 ?>
+
+<script type="text/javascript">
+	jQuery('.plgaction-btn').on('click', function () {
+		jQuery('#plgaction').val(jQuery(this).data('plgaction'));
+		jQuery('#task').val('plgrender.plgaction');
+
+		jQuery('#plgrender-form').submit();
+	});
+</script>
+
 
 <div id="j-sidebar-container" class="span2">
 	<?php echo $this->sidebar; ?>
 </div>
 <div id="j-main-container" class="span10">
-	<?php echo $this->view; ?>
+	<form
+		action="<?php echo $formAction; ?>"
+		method="post" enctype="multipart/form-data" name="adminForm" id="plgrender-form">
+		<?php echo $this->view; ?>
+
+		<input type="hidden" name="plgaction" value="" id="plgaction"/>
+		<input type="hidden" name="task" value="" id="task"/>
+	</form>
 </div>
